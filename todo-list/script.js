55cb700a585009d8cb2bbe0645d73ad8c9d603f7
Ekbora <salih.tansel7@gmail.com>
2023-02-16 17:53:09 +0100

    Updating the todo list

--- todo-list/script.js
+++ todo-list/script.js
@@ -1,11 +1,38 @@
-const todos = [];
-const field = document.getElementById("todo");
-const btn = document.getElementById("btn");
+//* Variables
+const input = document.getElementById("input");
+const addBtn = document.getElementById("addBtn");
+const todoContainer = document.getElementById("todoContainer");
 
-btn.addEventListener("click", () => {
-    const list = document.querySelector("#list");
-    todos.push(field.value);
-    field.value = '';
-    field.focus();
-    list.innerHTML = `<ul><li>${todos.join('</li><li>')}</li></ul>`;
-})
+//* Stores tasks
+let todos = [];
+
+//* Create a task
+addBtn.addEventListener('click', () => {
+    //* Push the task in the table
+    todos.push(input.value);
+
+    //* Create a new 'li' element
+    const newTodo = document.createElement('p');
+
+    //* Clear the field
+    input.value = '';
+
+    //* Create the task
+    for(let i in todos){
+        newTodo.innerHTML = `${todos[i]}`
+    };
+
+    //* Display the task
+    todoContainer.appendChild(newTodo);
+
+    //* Mark the task as completed
+    newTodo.addEventListener('click', () => {
+        newTodo.classList.add("todo-checked");
+
+        //* Delete the task
+        newTodo.addEventListener('click', () => {
+            todoContainer.removeChild(newTodo);
+        });
+    });
+
+});
